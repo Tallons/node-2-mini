ab3f2c87acf4b3d256f7be873713d751ab2242dd
Tallons <Tallons27@yahoo.com>
2020-03-03 16:10:24 -0700

    fixed .put

--- Server/index.js
+++ Server/index.js
@@ -7,7 +7,7 @@ app.use(express.json());
 
 app.get("/api/books", bc.read);
 app.post("/api/books", bc.create);
-app.put("/api/books/id", bc.update);
+app.put("/api/books/:id", bc.update);
 app.delete("/api/books/:id", bc.delete);
 
 const port = 4000;
